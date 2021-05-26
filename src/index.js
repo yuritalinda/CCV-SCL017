3360db568b100c37d3dc9ca2a50a2d7a0bf611ea
Yuritalinda <yuritalinda@gmail.com>
2021-05-26 11:40:09 -0400

    2do commit. Error de importacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import validator from './validator.js';
+import {validator} from './validator' ;
 
-console.log(validator);
+//Se añade un Event listener...
+document.getElementById("confirmar").addEventListener("click", getValueInput);
+
+//con la función para atrapar el numero en un array que incluye regex para omitir no digitos...
+function getValueInput() {
+  let inputValue = Array.from(document.getElementById("cc-number").value.replace(/\D/g, '')); 
+
+  validator(inputValue);
+
+  
+
+
+}
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
 const validator = {
-  // ...
-};
+  //algoritmo de luhn
 
-export default validator;
+ isValid = function(){
+  value = inputValue.reverse();
+
+  console.log(value);
+
+  alert("tenemos los numeros en un array, ahora vamos a validar");
+
+  return;
+
+}
+
+}
+  
+  export default validator;
